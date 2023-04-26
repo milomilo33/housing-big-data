64a0b4f05c14a04805162034de45a69b4814d957
milomilo33 <milovanovicm309@gmail.com>
2023-04-27 01:58:15 +0200

    Fully working first real-time query!

--- real-time-analysis/1.py
+++ real-time-analysis/1.py
@@ -1,22 +1,31 @@
+# 1. Most frequent type of crime in timeframe
+
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from os import environ
-
-def quiet_logs(sc):
-  logger = sc._jvm.org.apache.log4j
-  logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
-  logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
-
-spark = SparkSession \
-    .builder \
-    .appName("1. Hello world") \
-    .getOrCreate()
-
-quiet_logs(spark)
+import os
 
 KAFKA_BROKER = "kafka:9092"
 KAFKA_TOPIC = "crime-data"
+OUTPUT_PATH = "/home/housing-big-data/transformation-zone/most-frequent-crime"
+HDFS_NAMENODE_PATH = environ.get("CORE_CONF_fs_defaultFS", "hdfs://namenode:9000")
+
+def quiet_logs(sc):
+    logger = sc._jvm.org.apache.log4j
+    logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
+    logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+
+def write_batch_to_csv(df, epoch_id):
+    file_path = os.path.join(HDFS_NAMENODE_PATH + OUTPUT_PATH, f"batch_{epoch_id}.csv")
+    df.coalesce(1).write.mode("append").option("header", "true").csv(file_path)
+
+spark = SparkSession \
+    .builder \
+    .appName("1. Most frequent type of crime in timeframe") \
+    .getOrCreate()
+
+quiet_logs(spark)
 
 schema = StructType([
     StructField("key", StringType(), True),
@@ -41,24 +50,28 @@ crimes_stream = spark \
   .option("subscribe", KAFKA_TOPIC) \
   .load()
 
-crimes = crimes_stream.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
-                        .select(from_csv(col("value"), schema_str).alias("data")) \
-                        .select("data.*") \
+crimes = crimes_stream.selectExpr("timestamp", "CAST(key AS STRING)", "CAST(value AS STRING)") \
+                        .select("timestamp", from_csv(col("value"), schema_str).alias("data")) \
+                        .select("timestamp", "data.*") \
                         .drop('key', 'falls_within')
-# crimes = crimes_stream.selectExpr("CAST(value AS STRING)")
 
 query = crimes \
-    .writeStream \
-    .outputMode("append") \
+    .withWatermark("timestamp", "1 seconds") \
+    .groupBy(window("timestamp", "10 seconds", "3 seconds"), "crime_type") \
+    .agg(count("*").alias("crime_count")) \
+    .orderBy(desc('crime_count')) \
+    .limit(1)
+
+query.writeStream \
+    .outputMode("complete") \
     .format("console") \
     .option('truncate', 'false') \
     .start()
 
-# query = wordCounts \
-#     .writeStream \
-#     .outputMode("complete") \
-#     .format("console") \
-#     .trigger(processingTime='2 seconds') \
-#     .start()
+query.select('window.start', 'window.end', 'crime_type', 'crime_count') \
+    .writeStream \
+    .foreachBatch(write_batch_to_csv) \
+    .outputMode("complete") \
+    .start()
 
-query.awaitTermination()
+spark.streams.awaitAnyTermination()
---
+++ real-time-analysis/hello.py
@@ -0,0 +1,56 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+from os import environ
+
+def quiet_logs(sc):
+  logger = sc._jvm.org.apache.log4j
+  logger.LogManager.getLogger("org"). setLevel(logger.Level.ERROR)
+  logger.LogManager.getLogger("akka").setLevel(logger.Level.ERROR)
+
+spark = SparkSession \
+    .builder \
+    .appName("1. Hello world") \
+    .getOrCreate()
+
+quiet_logs(spark)
+
+KAFKA_BROKER = "kafka:9092"
+KAFKA_TOPIC = "crime-data"
+
+schema = StructType([
+    StructField("key", StringType(), True),
+    StructField("month", StringType(), True),
+    StructField("reported_by", StringType(), True),
+    StructField("falls_within", StringType(), True),
+    StructField("longitude", DoubleType(), True),
+    StructField("latitude", DoubleType(), True),
+    StructField("location", StringType(), True),
+    StructField("lsoa_code", StringType(), True),
+    StructField("lsoa_name", StringType(), True),
+    StructField("crime_type", StringType(), True),
+    StructField("last_outcome_category", StringType(), True),
+    StructField("context", StringType(), True),
+])
+schema_str = ",".join([f"{field.name} {field.dataType.typeName()}" for field in schema.fields])
+
+crimes_stream = spark \
+  .readStream \
+  .format("kafka") \
+  .option("kafka.bootstrap.servers", KAFKA_BROKER) \
+  .option("subscribe", KAFKA_TOPIC) \
+  .load()
+
+crimes = crimes_stream.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)") \
+                        .select(from_csv(col("value"), schema_str).alias("data")) \
+                        .select("data.*") \
+                        .drop('key', 'falls_within')
+
+query = crimes \
+    .writeStream \
+    .outputMode("append") \
+    .format("console") \
+    .option('truncate', 'false') \
+    .start()
+
+query.awaitTermination()
